15e2a9f754d0bdc36d17cc81626922f29c98905d
John <68535837+John-D-S@users.noreply.github.com>
2021-09-09 20:22:55 +1000

    plane now snaps back forward when the axis is 0

--- Assets/Mobile Plane/Scripts/PlaneController.cs
+++ Assets/Mobile Plane/Scripts/PlaneController.cs
@@ -20,24 +20,20 @@ public class PlaneController : MonoBehaviour
 
 	private void ApplyControlTorque(Vector2 _axis)
 	{
-		//Debug.Log(_axis);
-		if(_axis.magnitude > 0.001)
-		{
-			//calculating the rotation:
-			Vector3 targetRotUpdirection = Vector3.Lerp(cameraTransform.up, cameraTransform.rotation * new Vector3(-_axis.x, -_axis.y), _axis.magnitude);
-			Debug.DrawLine(transform.position, transform.position + targetRotUpdirection, Color.green);
-			Vector3 targetRotForwardDirection = (cameraTransform.rotation * Quaternion.Euler(_axis.y * maxTurnAngle, _axis.x * -maxTurnAngle, 0)) * Vector3.forward;
-			Quaternion targetRot = Quaternion.LookRotation(targetRotForwardDirection, targetRotUpdirection);
-			
-			//torque, with help from https://answers.unity.com/questions/171859/figuring-out-the-correct-amount-of-torque-to-apply.html:
-			Vector3 torqueF = OrientTorque(Quaternion.FromToRotation(transform.forward, targetRot * Vector3.forward).eulerAngles);
-			Vector3 torqueR = OrientTorque(Quaternion.FromToRotation(transform.right, targetRot * Vector3.right).eulerAngles);
-			Vector3 torqueU = OrientTorque(Quaternion.FromToRotation(transform.up, targetRot * Vector3.up).eulerAngles);
-			
-			Vector3 targetTorque = torqueF + torqueR + torqueU;
-            
-			rb.AddTorque(targetTorque * torque);
-		}
+		//calculating the rotation:
+		Vector3 targetRotUpdirection = Vector3.Lerp(cameraTransform.up, cameraTransform.rotation * new Vector3(-_axis.x, -_axis.y), _axis.magnitude);
+		Debug.DrawLine(transform.position, transform.position + targetRotUpdirection, Color.green);
+		Vector3 targetRotForwardDirection = (cameraTransform.rotation * Quaternion.Euler(_axis.y * maxTurnAngle, _axis.x * -maxTurnAngle, 0)) * Vector3.forward;
+		Quaternion targetRot = Quaternion.LookRotation(targetRotForwardDirection, targetRotUpdirection);
+		
+		//torque, with help from https://answers.unity.com/questions/171859/figuring-out-the-correct-amount-of-torque-to-apply.html:
+		Vector3 torqueF = OrientTorque(Quaternion.FromToRotation(transform.forward, targetRot * Vector3.forward).eulerAngles);
+		Vector3 torqueR = OrientTorque(Quaternion.FromToRotation(transform.right, targetRot * Vector3.right).eulerAngles);
+		Vector3 torqueU = OrientTorque(Quaternion.FromToRotation(transform.up, targetRot * Vector3.up).eulerAngles);
+		
+		Vector3 targetTorque = torqueF + torqueR + torqueU;
+        
+		rb.AddTorque(targetTorque * torque);
 	}
 	
 	private Vector3 OrientTorque(Vector3 _torque)
