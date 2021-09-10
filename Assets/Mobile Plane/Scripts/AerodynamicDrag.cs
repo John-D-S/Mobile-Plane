f38738a4ca7ad65ba9d9972b32aca13f8da29f55
John <68535837+John-D-S@users.noreply.github.com>
2021-09-10 12:02:03 +1000

    Added airplane drag

---
+++ Assets/Mobile Plane/Scripts/AerodynamicDrag.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AerodynamicDrag : MonoBehaviour
+{
+    [SerializeField] private Vector3 directionalDragFactor = Vector3.zero;
+
+    private Rigidbody rb;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void ApplyAerodynamicDrag()
+    {
+        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
+        Vector3 dragForce = Vector3.zero;
+        //dragForce.x = - directionalDragFactor.x * Mathf.Pow(localVelocity.x, 2) * 0.5f;
+        //dragForce.y = - directionalDragFactor.y * Mathf.Pow(localVelocity.y, 2) * 0.5f;
+        //dragForce.z = - directionalDragFactor.z * Mathf.Pow(localVelocity.z, 2) * 0.5f;
+        dragForce.x = - directionalDragFactor.x * localVelocity.x;
+        dragForce.y = - directionalDragFactor.y * localVelocity.y;
+        dragForce.z = - directionalDragFactor.z * localVelocity.z;
+        dragForce = transform.TransformDirection(dragForce);
+        Debug.DrawLine(transform.position, transform.position + dragForce, Color.red);
+        Debug.DrawLine(transform.position, transform.position + rb.velocity, Color.green);
+        rb.AddForce(dragForce, ForceMode.Acceleration);
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyAerodynamicDrag();
+    }
+}
