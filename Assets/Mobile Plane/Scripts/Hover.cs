229bedb489e5bfe6b133119f156fe96b2e9c4977
John <68535837+John-D-S@users.noreply.github.com>
2021-09-11 22:38:47 +1000

    finished house layout and added most furnature

--- Assets/Mobile Plane/Scripts/Hover.cs
+++ Assets/Mobile Plane/Scripts/Hover.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Hover : MonoBehaviour
 {
+    [SerializeField, Tooltip("Whether to apply the hover force in the global up direction or local up direction")] private bool applyLocalHoverForce;
+    [SerializeField, Tooltip("Whether to apply the hover force in the global up direction or local up direction")] private bool applyGlobalHoverForce;
     [SerializeField, Tooltip("The height above the object below it that this object will try to hover.")] private float targetHeight = 3;
     public float TargetHeight => targetHeight;
     [SerializeField, Tooltip("How much vertical drag will this gameObject have. This helps to prevent excessive bouncy oscillation.")] private float verticalDragModifier = 1;
@@ -11,21 +13,41 @@ public class Hover : MonoBehaviour
     /// <summary>
     /// The amount of upward force to apply to this gameobject
     /// </summary>
-    private float UpwardThrust
+    private Vector3 LocalUpwardThrust
     {
         get
         {
-            if(rb)
+            if(applyLocalHoverForce && rb)
             {
+                Vector3 forceDirection = transform.up;
                 RaycastHit hit = new RaycastHit();
                 // if the gameobject is above a collider, apply a force to hover.
-                if(Physics.Raycast(transform.position, Vector3.down, out hit,targetHeight * 2f, ~LayerMask.GetMask("Player")))
+                if(Physics.Raycast(transform.position, -forceDirection, out hit,targetHeight * 2f, ~LayerMask.GetMask("Player")))
                 {
                     float height = hit.distance;
-                    return Mathf.Abs(Physics.gravity.y) * targetHeight / height - rb.velocity.y * verticalDragModifier;
+                    return (Mathf.Abs(Physics.gravity.y) * targetHeight / height - rb.velocity.y * verticalDragModifier) * forceDirection;
                 }
             }
-            return 0;
+            return Vector3.zero;
+        }
+    }
+    
+    private Vector3 GlobalUpwardThrust
+    {
+        get
+        {
+            if(applyGlobalHoverForce && rb)
+            {
+                Vector3 forceDirection = Vector3.up;
+                RaycastHit hit = new RaycastHit();
+                // if the gameobject is above a collider, apply a force to hover.
+                if(Physics.Raycast(transform.position, -forceDirection, out hit,targetHeight * 2f, ~LayerMask.GetMask("Player")))
+                {
+                    float height = hit.distance;
+                    return (Mathf.Abs(Physics.gravity.y) * targetHeight / height - rb.velocity.y * verticalDragModifier) * forceDirection;
+                }
+            }
+            return Vector3.zero;
         }
     }
 
@@ -36,6 +58,6 @@ public class Hover : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.AddForce(Vector3.up* UpwardThrust);
+        rb.AddForce(LocalUpwardThrust + GlobalUpwardThrust);
     }
 }
